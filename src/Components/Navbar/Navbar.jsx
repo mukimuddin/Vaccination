52ce1c4ad9fa95be20471a44560e184b8c6c4f42
mukimuddin <mdmukimuddin.bd@gmail.com>
2024-10-25 23:36:29 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='main_menu'>
+       <div className="container">
+              <div className="menu_row">
+                     <div className="brand_logo">
+                            <img src="public/images/logo-1.png" alt="Logo" />
+                     </div>
+              </div>
+       </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
